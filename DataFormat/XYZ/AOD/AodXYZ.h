a6e24c46a0a0275cad995861ad024528a25166cd
bvonhall <barthelemy.von.haller@cern.ch>
2016-03-18 12:10:56 +0100

    Add DataFormat folder

---
+++ DataFormat/XYZ/AOD/AodXYZ.h
@@ -0,0 +1,27 @@
+///
+/// @file    AodXYZ.h
+/// @author  Barthelemy von Haller
+///
+
+#ifndef ALICE_O2_XYZ_AODXYZ_H
+#define ALICE_O2_XYZ_AODXYZ_H
+
+/// @brief    Here you put a short description of the namespace
+/// Extended documentation for this namespace
+/// @author  	Barthelemy von Haller
+namespace AliceO2 {
+namespace XYZ {
+
+/// @brief   Here you put a short description of the class
+/// Extended documentation for this class.
+/// @author 	Barthelemy von Haller
+class RawXYZ {
+public:
+
+
+};
+
+} // namespace XYZ
+} // namespace AliceO2
+
+#endif // ALICE_O2_XYZ_AODXYZ_H
